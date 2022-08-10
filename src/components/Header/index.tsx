140220449da3abe77aea13f3467af0d66d5ef568
yousaf wazir <umbrellaman612@gmail.com>
2022-08-10 12:57:41 +0100

    feat: fully added the header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,21 +1,34 @@
+import Link from 'next/link'
 import { SearchInput } from '../SearchInput'
 import { CreateAction } from './CreateAction'
 import { Logo } from './Logo'
 import { MenuHandler } from './MenuHandler'
 import { Notifications } from './Notifications'
 import { UserProfile } from './UserProfile'
+import { desktopHeaderOptions } from './values/desktopHeaderOptions'
 
 interface HeaderProps {}
 
 export const Header: React.FC<HeaderProps> = () => {
   return (
     <header className="w-full bg-[#24292F] h-16">
-      <nav className="w-full h-full max-w-screen-2xl mx-auto px-4 flex items-center justify-between">
+      <nav className="flex items-center justify-between w-full h-full px-4 mx-auto max-w-screen-2xl">
         <MenuHandler />
         <div className="flex items-center gap-3">
           <Logo />
           <div className="w-[300px] h-[30px] hidden md:block">
             <SearchInput />
+          </div>
+
+          {/** Desktop options */}
+          <div className="items-center hidden gap-1 md:flex">
+            {desktopHeaderOptions.map((Option) => (
+              <Link href={`/${Option.href}`} key={Option.title}>
+                <a className="text-sm font-bold text-white xl:p-1">
+                  {Option.title}
+                </a>
+              </Link>
+            ))}
           </div>
         </div>
         <div className="flex gap-1">
---
+++ src/components/Header/values/desktopHeaderOptions.ts
@@ -0,0 +1,21 @@
+export const desktopHeaderOptions: {
+  title: string
+  href: string
+}[] = [
+  {
+    title: 'Pull requests',
+    href: 'pulls',
+  },
+  {
+    title: 'Issues',
+    href: 'issues',
+  },
+  {
+    title: 'Marketplace',
+    href: 'marketplace',
+  },
+  {
+    title: 'Explore',
+    href: 'explore',
+  },
+]
