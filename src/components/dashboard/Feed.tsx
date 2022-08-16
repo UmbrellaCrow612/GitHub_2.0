762b4409854646a168a0ef9a1f4ca53fb0ed422e
yousaf wazir <umbrellaman612@gmail.com>
2022-08-16 10:34:05 +0100

    feat: find repo added

--- src/components/dashboard/Feed.tsx
+++ src/components/dashboard/Feed.tsx
@@ -2,7 +2,7 @@ interface FeedProps {}
 
 export const Feed: React.FC<FeedProps> = () => {
   return (
-    <div className="w-full px-4 border border-orange-400 py-7">
+    <div className="w-full px-4 border py-7 bg-[#F6F8FB]">
       <h3 className="w-full text-2xl font-semibold tracking-wide">
         The home for all developers — including you.
       </h3>
@@ -11,6 +11,20 @@ export const Feed: React.FC<FeedProps> = () => {
         to how GitHub works, tools to help you build software, and help merging
         your first lines of code.
       </p>
+
+      {/** Repo */}
+
+      <div className="mt-3">
+        <p className="text-base text-gray-800">Repositories</p>
+        {/** Repos */}
+        <div className="border-gray-100 border-[0.1em] bg-[#FFFFFF] shadow-sm rounded-sm mt-1 px-2 py-6">
+          <input
+            type="text"
+            className="bg-[#F6F8FB] w-full border-[0.1em] border-[#CFD8DE] rounded-md px-3 py-1 placeholder:text-gray-500 placeholder:text-sm"
+            placeholder="Find a repository..."
+          />
+        </div>
+      </div>
     </div>
   )
 }
